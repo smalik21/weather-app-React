12cc78bf56e2b6eb22d900649bd458c276594aac
smalik21 <sahilmalik25007@gmail.com>
2023-12-19 13:42:05 +0530

    fix design faults

--- src/components/weather_card/current/Place.js
+++ src/components/weather_card/current/Place.js
@@ -3,8 +3,10 @@ function Place(props) {
     return (
         <div className="place">
             <p className="location">{props.location}</p>
-            <p className="region">{props.region}</p>
-            <p className="country">{props.country}</p>
+            <div className="region-country">
+                <p className="region">{props.region}</p>
+                <p className="country">{props.country}</p>
+            </div>
         </div>
     )
 }
